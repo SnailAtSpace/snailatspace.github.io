e0f6c164bce04f3814e45a497331933d4e807d5f
SnailAtSpace <mark.shtarkmanbor@gmail.com>
2026-01-06 21:07:09 +0800

    initial commit

---
+++ precache-manifest.a6985a8818478b7794b024b29ce7015d.js
@@ -0,0 +1,682 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd000c9a709aa60c6b0",
+    "url": "/css/app.9d143f2f.css"
+  },
+  {
+    "revision": "8f2a6b88ab03a58544db",
+    "url": "/css/chunk-vendors.2f865879.css"
+  },
+  {
+    "revision": "53ce8c8840322a10f92c5828d3221e22",
+    "url": "/fonts/Consolas.53ce8c88.ttf"
+  },
+  {
+    "revision": "d595d94bceb01058b3d34957a41ffb4b",
+    "url": "/fonts/Helvetica-Black.d595d94b.otf"
+  },
+  {
+    "revision": "fd850139b4cf133f3a26fcc195cba595",
+    "url": "/fonts/Helvetica-Bold.fd850139.otf"
+  },
+  {
+    "revision": "4376652caec8a8cd4024347f48b1330f",
+    "url": "/fonts/Helvetica-Light.4376652c.otf"
+  },
+  {
+    "revision": "bc6fdf7cd9e3267ff6168363266d0a19",
+    "url": "/fonts/Helvetica-LightOblique.bc6fdf7c.otf"
+  },
+  {
+    "revision": "3c89649614630d9bf36deb57c9a599a8",
+    "url": "/fonts/Helvetica.3c896496.otf"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/fonts/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "0bae535590ffdc6ffb2cfa45b8fb2ae3",
+    "url": "/fonts/PxPlus_IBM_VGA8.0bae5355.ttf"
+  },
+  {
+    "revision": "ca04717077f5278b7fbb28d47ccd89a5",
+    "url": "/fonts/compcon.ca047170.ttf"
+  },
+  {
+    "revision": "0fb040cb4149d1895c7992af7b8f2f54",
+    "url": "/fonts/materialdesignicons-webfont.0fb040cb.woff2"
+  },
+  {
+    "revision": "1514bb9f34d706d477c19fedd78fc417",
+    "url": "/fonts/materialdesignicons-webfont.1514bb9f.ttf"
+  },
+  {
+    "revision": "5a409f9ffec6a612a6c056e7b8990337",
+    "url": "/fonts/materialdesignicons-webfont.5a409f9f.woff"
+  },
+  {
+    "revision": "ff90567bbebd6839c63922afa497cbed",
+    "url": "/fonts/materialdesignicons-webfont.ff90567b.eot"
+  },
+  {
+    "revision": "6f2823d273ff49b39a086d602227267e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccd000c9a709aa60c6b0",
+    "url": "/js/app.a79df933.js"
+  },
+  {
+    "revision": "61f0abd586d44b8c880d",
+    "url": "/js/chunk-05005093.8958bc42.js"
+  },
+  {
+    "revision": "aaf2168cf89ffdfa29be",
+    "url": "/js/chunk-05e068a4.1cbd60c8.js"
+  },
+  {
+    "revision": "b616e7c19372e3c85644",
+    "url": "/js/chunk-0a2a393c.6faba2f7.js"
+  },
+  {
+    "revision": "56d67467d6111e51fbd9",
+    "url": "/js/chunk-1603621f.63295440.js"
+  },
+  {
+    "revision": "b147d3e3923586799f20",
+    "url": "/js/chunk-2d0a4629.9ad29b78.js"
+  },
+  {
+    "revision": "064e4c81ff82ac78f407",
+    "url": "/js/chunk-2d0abfcf.b1948847.js"
+  },
+  {
+    "revision": "03126d7af1b038a3b305",
+    "url": "/js/chunk-2d0b722b.13cf7afc.js"
+  },
+  {
+    "revision": "1a5cd015dbaab39c62d5",
+    "url": "/js/chunk-2d0d7838.c2ec399c.js"
+  },
+  {
+    "revision": "4098149070734c030c2c",
+    "url": "/js/chunk-2d0dd0bc.b5c4bb06.js"
+  },
+  {
+    "revision": "eb5511ed675d63a49635",
+    "url": "/js/chunk-2d208840.bfde29bb.js"
+  },
+  {
+    "revision": "c7e8dce9a5a7b5d144d3",
+    "url": "/js/chunk-2d208de0.10d1f0f6.js"
+  },
+  {
+    "revision": "6ea18b80afd69dff085b",
+    "url": "/js/chunk-2d2105ef.924ed2c2.js"
+  },
+  {
+    "revision": "14c9a416231838873a2a",
+    "url": "/js/chunk-2d226365.cbf08ae4.js"
+  },
+  {
+    "revision": "6db89f1f721b926525a7",
+    "url": "/js/chunk-2d228cf2.4f17031f.js"
+  },
+  {
+    "revision": "56894df5dbcfff58d00c",
+    "url": "/js/chunk-2d22c8fe.61fc0767.js"
+  },
+  {
+    "revision": "771a692f65306b770a43",
+    "url": "/js/chunk-2d22dbc9.e5f47d9a.js"
+  },
+  {
+    "revision": "fd6a94fde498b4a8749a",
+    "url": "/js/chunk-2dcd6fd6.e8d043fd.js"
+  },
+  {
+    "revision": "56977f455e90a6b9f37d",
+    "url": "/js/chunk-3d7c4cff.de956be5.js"
+  },
+  {
+    "revision": "62bab0826b0340c755a8",
+    "url": "/js/chunk-4d9e9f76.e17eab56.js"
+  },
+  {
+    "revision": "b46c3b671120e287a7e4",
+    "url": "/js/chunk-5f19badc.998c797b.js"
+  },
+  {
+    "revision": "3c348eaf3754fe907838",
+    "url": "/js/chunk-669d28db.e315b24e.js"
+  },
+  {
+    "revision": "a79f23e63fc11ea1ee2a",
+    "url": "/js/chunk-749c9e96.1de435a2.js"
+  },
+  {
+    "revision": "2e20a4a6f6520065c6b6",
+    "url": "/js/chunk-765474a9.9c0add33.js"
+  },
+  {
+    "revision": "cf1ce7bc62702f397119",
+    "url": "/js/chunk-76f19ef7.941174e3.js"
+  },
+  {
+    "revision": "8aa48e572042b00ba796",
+    "url": "/js/chunk-78511423.539cd877.js"
+  },
+  {
+    "revision": "aae5a15f85f8f5b531b3",
+    "url": "/js/chunk-7c61a60d.f8733c1f.js"
+  },
+  {
+    "revision": "a91847043d49f17c38e4",
+    "url": "/js/chunk-7c8c0a62.eb686ee2.js"
+  },
+  {
+    "revision": "99110245cb8ea0a8e997",
+    "url": "/js/chunk-97919c90.e8a5da24.js"
+  },
+  {
+    "revision": "49f35e389b7c3bed9dce",
+    "url": "/js/chunk-a2fda386.0f99b892.js"
+  },
+  {
+    "revision": "d972cf0c630af0df9234",
+    "url": "/js/chunk-a8864f42.60a9073a.js"
+  },
+  {
+    "revision": "d68a15af258a25ab7edf",
+    "url": "/js/chunk-e5bb031e.5624eab1.js"
+  },
+  {
+    "revision": "cfb278039b0350ff337c",
+    "url": "/js/chunk-ef0c5be6.79e0f364.js"
+  },
+  {
+    "revision": "f4b83fb64702bdb54a90",
+    "url": "/js/chunk-f969a952.9bc39492.js"
+  },
+  {
+    "revision": "8f2a6b88ab03a58544db",
+    "url": "/js/chunk-vendors.2dead854.js"
+  },
+  {
+    "revision": "14abca010b7890f05013",
+    "url": "/js/polyfills-core-js.c802f76e.js"
+  },
+  {
+    "revision": "ea792d18520bc07e7b86",
+    "url": "/js/polyfills-css-shim.ac087954.js"
+  },
+  {
+    "revision": "d381194a75412f4897f1",
+    "url": "/js/polyfills-dom.e01853a0.js"
+  },
+  {
+    "revision": "e2fec67faee5cda6e809b83d4244a00a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3eb9214e6c170af281b7338874f728c6",
+    "url": "/static/img/frame/nodata.png"
+  },
+  {
+    "revision": "335d8f62ed27d89213cb74160c39a1b7",
+    "url": "/static/img/logo/albatross.svg"
+  },
+  {
+    "revision": "669168b0890cdbea35e9605dceb7099f",
+    "url": "/static/img/logo/aun.svg"
+  },
+  {
+    "revision": "066e1290c7fb1c4a3aab591afc6214c1",
+    "url": "/static/img/logo/barony.svg"
+  },
+  {
+    "revision": "fc9f954775e5e4d9984c4a6f3b3adbbf",
+    "url": "/static/img/logo/gms.svg"
+  },
+  {
+    "revision": "1ab055f050957462cd71bb8707802641",
+    "url": "/static/img/logo/ha.svg"
+  },
+  {
+    "revision": "edfee36f7a9020f43e71c81589079e0f",
+    "url": "/static/img/logo/horus.svg"
+  },
+  {
+    "revision": "cfba95a84c021ba936b638d45a4760cc",
+    "url": "/static/img/logo/ips-n.svg"
+  },
+  {
+    "revision": "8e4c51bac6fcdcf42a9e59dfe66ac804",
+    "url": "/static/img/logo/sparri.svg"
+  },
+  {
+    "revision": "a15ea29bd33a53e567ae9ff828c25a01",
+    "url": "/static/img/logo/ssc.svg"
+  },
+  {
+    "revision": "fc9f954775e5e4d9984c4a6f3b3adbbf",
+    "url": "/static/img/logo/union.svg"
+  },
+  {
+    "revision": "a0625f05f402ae07d01d5f846e70c475",
+    "url": "/static/img/logo/voladores.svg"
+  },
+  {
+    "revision": "b76411bac170bbdccef1b28f471e7bd5",
+    "url": "/static/img/map/nodata.png"
+  },
+  {
+    "revision": "1e342c15c42b0ca312cd912436a982a6",
+    "url": "/static/img/mech/alt_agonist.png"
+  },
+  {
+    "revision": "611367a34f24c289421a72d0ebcc9deb",
+    "url": "/static/img/mech/alt_hercules.png"
+  },
+  {
+    "revision": "a096e4f13344e6086c763d6468c5ede6",
+    "url": "/static/img/mech/alt_hortuga.png"
+  },
+  {
+    "revision": "8aaa661d55ca4575305ad9e565f07fee",
+    "url": "/static/img/mech/alt_sherman_old.png"
+  },
+  {
+    "revision": "f1762c12cb4e70af91608c7fd86f4ecc",
+    "url": "/static/img/mech/mf_atlas.png"
+  },
+  {
+    "revision": "7ca7e16e520d127cd83346ce36337c87",
+    "url": "/static/img/mech/mf_balor.png"
+  },
+  {
+    "revision": "66cd25128fd1d9f1bf1141914cf75ca7",
+    "url": "/static/img/mech/mf_barbarossa.png"
+  },
+  {
+    "revision": "92cfd25e3665083a8614344b4effd151",
+    "url": "/static/img/mech/mf_black_witch.png"
+  },
+  {
+    "revision": "166d69559f3cf9a7f9d3acd20e4071cb",
+    "url": "/static/img/mech/mf_blackbeard.png"
+  },
+  {
+    "revision": "43b5564f74e69e9a734e024d10f169ac",
+    "url": "/static/img/mech/mf_deaths_head.png"
+  },
+  {
+    "revision": "38e5a952f919cdf88459e45b20084e23",
+    "url": "/static/img/mech/mf_drake.png"
+  },
+  {
+    "revision": "475e61a64d25ed163917d91f8bd171c5",
+    "url": "/static/img/mech/mf_dusk_wing.png"
+  },
+  {
+    "revision": "f2183fe001e5d014e7c0d9dcff46e2c8",
+    "url": "/static/img/mech/mf_genghis.png"
+  },
+  {
+    "revision": "9b986a1bdfd7c39865c97f1bc8d48808",
+    "url": "/static/img/mech/mf_goblin.png"
+  },
+  {
+    "revision": "c6d07704ebd0ae0a8ef695b06ca4387e",
+    "url": "/static/img/mech/mf_gorgon.png"
+  },
+  {
+    "revision": "19e902c4431263d1ad1c3c8909c6a2ce",
+    "url": "/static/img/mech/mf_hydra.png"
+  },
+  {
+    "revision": "36906096ffc29505ce40a7fbb7c60aad",
+    "url": "/static/img/mech/mf_iskander.png"
+  },
+  {
+    "revision": "6cfdb8caffddc30d1dbd7fc299058971",
+    "url": "/static/img/mech/mf_kobold.png"
+  },
+  {
+    "revision": "1e9173ae6695e8ee8803a764bc0fa971",
+    "url": "/static/img/mech/mf_lancaster.png"
+  },
+  {
+    "revision": "eec05e69a482a8a90b0986d57af34048",
+    "url": "/static/img/mech/mf_lich.png"
+  },
+  {
+    "revision": "e23a3a97dbd2295c460d3df8ebc7e424",
+    "url": "/static/img/mech/mf_manticore.png"
+  },
+  {
+    "revision": "fce1dbc94ac0c6610be1766637a1fb93",
+    "url": "/static/img/mech/mf_metalmark.png"
+  },
+  {
+    "revision": "c6ce3617bad1ae0c078ce5588a51d5a0",
+    "url": "/static/img/mech/mf_minotaur.png"
+  },
+  {
+    "revision": "59302f02765dcefaad4cc13f96b3709b",
+    "url": "/static/img/mech/mf_monarch.png"
+  },
+  {
+    "revision": "5b41fa00b32bee7a3308cc741fc566f0",
+    "url": "/static/img/mech/mf_mourning_cloak.png"
+  },
+  {
+    "revision": "05bb1ef2c04e2cfd37723ea39e685e5e",
+    "url": "/static/img/mech/mf_napoleon.png"
+  },
+  {
+    "revision": "78cfa9e42751006bf066589645d526e8",
+    "url": "/static/img/mech/mf_nelson.png"
+  },
+  {
+    "revision": "6758f13035740c4652f3466b1d5f606d",
+    "url": "/static/img/mech/mf_pegasus.png"
+  },
+  {
+    "revision": "ee7e90221840cb370d0f271ff4c26e60",
+    "url": "/static/img/mech/mf_raleigh.png"
+  },
+  {
+    "revision": "4f5ec332c5c605ce3b0f749175873dbf",
+    "url": "/static/img/mech/mf_saladin.png"
+  },
+  {
+    "revision": "6174466c93e61058aba0311832feb8a1",
+    "url": "/static/img/mech/mf_sherman.png"
+  },
+  {
+    "revision": "2a47f8104e70022d5e9c549a99eb290c",
+    "url": "/static/img/mech/mf_standard_pattern_i_everest.png"
+  },
+  {
+    "revision": "51ccc65a58e853d8eea1d03b19e57cc6",
+    "url": "/static/img/mech/mf_sunzi.png"
+  },
+  {
+    "revision": "40b4f804b4bb9e736a020ffe3aab79d0",
+    "url": "/static/img/mech/mf_swallowtail.png"
+  },
+  {
+    "revision": "d9f0baeff762d0c9dc47c7ecbd6d5d86",
+    "url": "/static/img/mech/mf_tokugawa.png"
+  },
+  {
+    "revision": "41823c4a9f95ea062cecb32eed855414",
+    "url": "/static/img/mech/mf_tortuga.png"
+  },
+  {
+    "revision": "fd6327cc0f5c243d428ec88514070a68",
+    "url": "/static/img/mech/mf_vlad.png"
+  },
+  {
+    "revision": "5bcfea61b557042a6d5bfeb5bff6d8c1",
+    "url": "/static/img/mech/mf_zheng.png"
+  },
+  {
+    "revision": "36c9c281c79dabf2a51c04414eddeed0",
+    "url": "/static/img/mech/pre_assault_pixel.png"
+  },
+  {
+    "revision": "54a331ef362d54325401c2c8898f8932",
+    "url": "/static/img/mech/pre_hacker_pixel.png"
+  },
+  {
+    "revision": "cca8d01384c308d59748fba245a74602",
+    "url": "/static/img/mech/pre_sniper_pixel.png"
+  },
+  {
+    "revision": "3176b558ed22f6c3cee86a264c34fca8",
+    "url": "/static/img/mech/pre_support_pixel.png"
+  },
+  {
+    "revision": "b33f8fe2f8c7324dc195c51bc3a35506",
+    "url": "/static/img/mech/pre_vanguard_pixel.png"
+  },
+  {
+    "revision": "2f263d5acccce4cf615081e90f116602",
+    "url": "/static/img/mech/thunderhead.PNG"
+  },
+  {
+    "revision": "63074e49d56b09ac909f6797c505984b",
+    "url": "/static/img/misc/massif_logo.png"
+  },
+  {
+    "revision": "fa0eb96b14994886132f1f81cdd45f52",
+    "url": "/static/img/misc/patreon_tiers/Cosmopolitan.svg"
+  },
+  {
+    "revision": "d2a8fc1fdc2aa646afa45ad8f7b80b31",
+    "url": "/static/img/misc/patreon_tiers/Diasporan.svg"
+  },
+  {
+    "revision": "16a774dfa5ef3404b005b395b00d7709",
+    "url": "/static/img/misc/patreon_tiers/Honorary.svg"
+  },
+  {
+    "revision": "7b056b48cac4831fc4ce27258a7c6d67",
+    "url": "/static/img/misc/patreon_tiers/Lancer.svg"
+  },
+  {
+    "revision": "a76d1010078ebb9dcb1b725d89224276",
+    "url": "/static/img/misc/patreon_tiers/MONIST.svg"
+  },
+  {
+    "revision": "e39a60992eb1f7df0f3699296f029710",
+    "url": "/static/img/misc/patreon_tiers/NHP.svg"
+  },
+  {
+    "revision": "6d9e8c0e8a1f32e84af39d0ad31f8eb9",
+    "url": "/static/img/misc/patreon_tiers/SpecialThanks.svg"
+  },
+  {
+    "revision": "055b069c29d7a70b0a66e90a4bddfffa",
+    "url": "/static/img/misc/retrograde_logo.png"
+  },
+  {
+    "revision": "1ddeb5ee7e69fa5ea41f77c50f84e2c5",
+    "url": "/static/img/npc/nodata.png"
+  },
+  {
+    "revision": "d4f9d38d72805e2fa6d7dfb13b4caadb",
+    "url": "/static/img/pilot/nodata.png"
+  },
+  {
+    "revision": "59489e907712827d9dd750e92f499048",
+    "url": "/static/img/pilot/wampire_1.PNG"
+  },
+  {
+    "revision": "5eaa4b81046dd8dd7be5b63e9e396214",
+    "url": "/static/img/pilot/wampire_10.PNG"
+  },
+  {
+    "revision": "e49704309168d99b04a53c447f8b972a",
+    "url": "/static/img/pilot/wampire_11.PNG"
+  },
+  {
+    "revision": "da0983ea7d706ae8757d4732bb040fd6",
+    "url": "/static/img/pilot/wampire_2.PNG"
+  },
+  {
+    "revision": "378ef27d0ffd73d9e4c50fd2522da374",
+    "url": "/static/img/pilot/wampire_3.png"
+  },
+  {
+    "revision": "c28b0bcd622d6eb042292e25029de62f",
+    "url": "/static/img/pilot/wampire_4.PNG"
+  },
+  {
+    "revision": "dce02c126464fb11f023e95b7748348b",
+    "url": "/static/img/pilot/wampire_5.PNG"
+  },
+  {
+    "revision": "3db6dcc71819f2359aa5d87149489670",
+    "url": "/static/img/pilot/wampire_6.PNG"
+  },
+  {
+    "revision": "a2f2c82176ed2b08698fd4cf9405f107",
+    "url": "/static/img/pilot/wampire_7.PNG"
+  },
+  {
+    "revision": "d40f9102ca6afaf50c6f68c63b955490",
+    "url": "/static/img/pilot/wampire_8.PNG"
+  },
+  {
+    "revision": "e046fca554d2cfc43fb1e3cab8021479",
+    "url": "/static/img/pilot/wampire_9.png"
+  },
+  {
+    "revision": "04662d5f78c7218ce60401fcae3030c9",
+    "url": "/static/img/talent/ACE.svg"
+  },
+  {
+    "revision": "5f79a069ee16ba1ae584f6343a4cd352",
+    "url": "/static/img/talent/BLACK THUMB.svg"
+  },
+  {
+    "revision": "095b19aa9a3d7f1fe1487fef50460df1",
+    "url": "/static/img/talent/BONDED.svg"
+  },
+  {
+    "revision": "b8d1946d63a7f1b51efd039d6e752dc2",
+    "url": "/static/img/talent/BRAWLER.svg"
+  },
+  {
+    "revision": "99b43f8751c7a608d0277dbe3c05af1b",
+    "url": "/static/img/talent/BRUTAL.svg"
+  },
+  {
+    "revision": "38889a767b6812b0874b119118529f5c",
+    "url": "/static/img/talent/CENTIMANE.svg"
+  },
+  {
+    "revision": "2fe49d0a3692d5b6973be3471aa8db14",
+    "url": "/static/img/talent/COMBINED ARMS.svg"
+  },
+  {
+    "revision": "e71f8252f0bf035956f2fca97a1e61bc",
+    "url": "/static/img/talent/CRACK SHOT.svg"
+  },
+  {
+    "revision": "ae93dbd750f3aa8f9c1a1e401fa40348",
+    "url": "/static/img/talent/DRONE COMMANDER.svg"
+  },
+  {
+    "revision": "2e6f78978a46f366598ed0f5a57e6b83",
+    "url": "/static/img/talent/DUELIST.svg"
+  },
+  {
+    "revision": "3c30bc43bf3f48d87a448047616ad4d1",
+    "url": "/static/img/talent/ENGINEER.svg"
+  },
+  {
+    "revision": "6ac8fd00b18e5c19655e018869cba014",
+    "url": "/static/img/talent/EXECUTIONER.svg"
+  },
+  {
+    "revision": "32e85442f9db0796e57a63644027fb70",
+    "url": "/static/img/talent/EXEMPLAR.svg"
+  },
+  {
+    "revision": "7dbf0c5d09c4cb59cf7482ee72fe8dd5",
+    "url": "/static/img/talent/GENERIC TALENT.svg"
+  },
+  {
+    "revision": "7e9428e6fcf6adea2dba38a10bb959d4",
+    "url": "/static/img/talent/GREASE MONKEY.svg"
+  },
+  {
+    "revision": "38994c22da50fcb47da8dd2763ca8828",
+    "url": "/static/img/talent/GUNSLINGER.svg"
+  },
+  {
+    "revision": "11c4a387d4c48117205568fac972b1d3",
+    "url": "/static/img/talent/HACKER.svg"
+  },
+  {
+    "revision": "14e2bee7f19ceeb13e894ef67057cedf",
+    "url": "/static/img/talent/HEAVY GUNNER.svg"
+  },
+  {
+    "revision": "f5467e19cc9413ce16a6d70893bcd757",
+    "url": "/static/img/talent/HUNTER.svg"
+  },
+  {
+    "revision": "4ce3efa1c0bb78c5a764313d67a2a8a5",
+    "url": "/static/img/talent/INFILTRATOR.svg"
+  },
+  {
+    "revision": "573bf37bc2cc90e3c00c7c6f2c365185",
+    "url": "/static/img/talent/JUGGERNAUT.svg"
+  },
+  {
+    "revision": "f9b8073d25fdca08b55250673c6edf20",
+    "url": "/static/img/talent/LEADER.svg"
+  },
+  {
+    "revision": "8283619b568f80c70b609e5722175ad0",
+    "url": "/static/img/talent/NUCLEAR CAVALIER.svg"
+  },
+  {
+    "revision": "f0b267fc5d9b74f05aa865ab1d0f3faa",
+    "url": "/static/img/talent/SIEGE SPECIALIST.svg"
+  },
+  {
+    "revision": "8902da60b84fe6619a21cfb911b1b5ba",
+    "url": "/static/img/talent/SKIRMISHER.svg"
+  },
+  {
+    "revision": "047462b0028c0099d8750ea8d23c256e",
+    "url": "/static/img/talent/SPACEBORN.svg"
+  },
+  {
+    "revision": "2c32c6671a2f076f9731b3a85b4cc1e6",
+    "url": "/static/img/talent/SPOTTER.svg"
+  },
+  {
+    "revision": "1cf0e4e1ff3706d886b2e1898d27f643",
+    "url": "/static/img/talent/STORMBRINGER.svg"
+  },
+  {
+    "revision": "a0f9c47907686523d100971a47308d5f",
+    "url": "/static/img/talent/TACTICIAN.svg"
+  },
+  {
+    "revision": "d77e23f9bea05c794a94909c4c03c550",
+    "url": "/static/img/talent/TECHNOPHILE.svg"
+  },
+  {
+    "revision": "ae36a1a14403a111c45ea762d80a92f7",
+    "url": "/static/img/talent/VANGUARD.svg"
+  },
+  {
+    "revision": "203a948614a06bea6fbfbdf1b11b3ecd",
+    "url": "/static/img/talent/WALKING ARMORY.svg"
+  }
+]);
